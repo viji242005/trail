df74456afdfd43cc88656bda066fa3c9a0f3756f
viji242005 <vijayalakshmi24092005@gmail.com>
2026-04-07 11:37:28 +0530

    add line

--- trial2.py
+++ trial2.py
@@ -3,3 +3,4 @@ b = int(input("Enter another number: "))
 
 print ("The sum of", a, "and", b, "is:", a + b)
 print ("The difference of", a, "and", b, "is:", a - b)
+print ("The product of", a, "and", b, "is:", a * b)
