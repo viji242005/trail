27fe7ea3534df71889e995f068ad684f52c8cb14
viji242005 <vijayalakshmi24092005@gmail.com>
2026-04-07 11:33:33 +0530

    added a line

--- trial2.py
+++ trial2.py
@@ -3,3 +3,6 @@ b = int(input("Enter another number: "))
 
 print ("The sum of", a, "and", b, "is:", a + b)
 print ("The difference of", a, "and", b, "is:", a - b)
+print ("The product of", a, "and", b, "is:", a * b)
+if b != 0:
+    print ("The quotient of", a, "and", b, "is:", a / b)
